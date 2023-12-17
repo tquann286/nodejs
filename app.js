7ee4a60c31ae102958db0c6ef7630aaa0f58a1e6
tquann286 <quantrung286@gmail.com>
2023-12-17 21:04:06 +0700

    init

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const http = require('http');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const routes = require('./routes');
+const app = express()
 
-console.log(routes.someText);
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-const server = http.createServer(routes.handler);
+app.use(bodyParser.urlencoded({ extended: false }))
 
-server.listen(3000);
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+  res.status(404).send("404")
+})
+
+app.listen(3000)
