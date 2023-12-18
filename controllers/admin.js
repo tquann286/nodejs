60d5dd030f34f4a32d2a93e28caee2275c639cd2
tquann286 <quantrung286@gmail.com>
2023-12-18 22:00:02 +0700

    Refactor admin product forms and add new products to database
    Update admin product forms to handle editing and adding new products.
    Also, add a new product to the database. These changes aim to improve
    the user experience and provide better functionality for managing
    products. No associated issues.

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,6 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
+    editing: false,
     formsCSS: true,
     productCSS: true,
     activeAddProduct: true,
@@ -25,10 +26,17 @@ exports.getEditProduct = (req, res, next) => {
   if (!editMode) {
     return res.redirect('/')
   }
-  res.render('admin/edit-product', {
-    pageTitle: 'Edit Product',
-    path: '/admin/edit-product',
-    editing: editMode === 'true',
+  const prodId = req.params.productId
+  Product.findById(prodId, (product) => {
+    if (!product) return res.redirect('/')
+    console.log('product: ', product)
+
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode === 'true',
+      product,
+    })
   })
 }
 
