cfdd9f9e37e6bb50c91c58dbd9059ef92a3e60e7
tquann286 <quantrung286@gmail.com>
2023-12-20 21:41:31 +0700

    131 postCartDeleteProduct and update product data

--- controllers/shop.js
+++ controllers/shop.js
@@ -59,6 +59,14 @@ exports.postCart = (req, res, next) => {
   res.redirect('/')
 }
 
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId
+  Product.findById(prodId, (product) => {
+    Cart.deleteProduct(prodId, product.price)
+    res.redirect('/cart')
+  })
+}
+
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
