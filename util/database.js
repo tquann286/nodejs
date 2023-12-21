15df2d044b0f28c3e05743b04f1ececb13dc87e6
tquann286 <quantrung286@gmail.com>
2023-12-21 17:05:56 +0700

    154  Refactor database connection setup and app initialization

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize')
 const sequelize = new Sequelize({
   host: '127.0.0.1',
   port: '3307',
-  user: 'root',
+  username: 'root',
   database: 'node-complete',
   password: 'Trungquan2806',
   dialect: 'mysql'
