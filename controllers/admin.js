3ece50ab2f8c1d9273a37fc922cc35d5148bd9d3
tquann286 <quantrung286@gmail.com>
2023-12-21 22:07:00 +0700

    165

--- controllers/admin.js
+++ controllers/admin.js
@@ -90,7 +90,7 @@ exports.postDeleteProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
     .then((products) => {
       res.render('admin/products', {
         prods: products,
