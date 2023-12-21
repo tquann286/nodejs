c5624916c4838fb202a8ebaa359ffca44be6966c
tquann286 <quantrung286@gmail.com>
2023-12-21 14:31:09 +0700

    142  Refactor database connection, execute query asynchronously, and format JSON data

--- app.js
+++ app.js
@@ -14,7 +14,11 @@ app.set('views', 'views')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
-db.execute('SELECT * FROM products')
+db.execute('SELECT * FROM products').then(result => {
+  console.log('result: ', result[0])
+}).catch((err) => {
+  console.log(err)
+})
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
--- util/database.js
+++ util/database.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2')
 
 const pool = mysql.createPool({
   host: '127.0.0.1',
+  port: '3307',
   user: 'root',
   database: 'node-complete',
   password: 'Trungquan2806',
