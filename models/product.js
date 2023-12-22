c94f45425348d5667d62b9b512784fbf20a3137a
tquann286 <quantrung286@gmail.com>
2023-12-22 21:40:47 +0700

    189

--- models/product.js
+++ models/product.js
@@ -2,21 +2,27 @@ const getDb = require('../util/database').getDb
 const ObjectId = require('mongodb').ObjectId
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title
     this.price = price
     this.description = description
     this.imageUrl = imageUrl
+    this._id = id
   }
 
   save() {
     const db = getDb()
-    return db
-      .collection('products')
-      .insertOne(this)
-      .catch((err) => {
-        console.log(err)
-      })
+    let dbOp
+    const { _id, ...updatedProduct } = this
+    if (this._id) {
+      dbOp = db.collection('products').updateOne({ _id: new ObjectId(this._id) }, { $set: updatedProduct  })
+    } else {
+      dbOp = db.collection('products').insertOne(this)
+    }
+
+    return dbOp.catch((err) => {
+      console.log(err)
+    })
   }
 
   static fetchAll() {
@@ -30,7 +36,10 @@ class Product {
 
   static findById(prodId) {
     const db = getDb()
-    return db.collection('products').find({ _id: new ObjectId(prodId) }).next()
+    return db
+      .collection('products')
+      .find({ _id: new ObjectId(prodId) })
+      .next()
   }
 }
 
