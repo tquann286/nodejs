0994ae4131c41f7564371eb2f42f2cf51ec0ef78
tquann286 <quantrung286@gmail.com>
2023-12-22 17:14:21 +0700

    173

--- controllers/shop.js
+++ controllers/shop.js
@@ -101,9 +101,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 }
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart
       return cart.getProducts()
     })
     .then((products) =>
@@ -115,23 +117,18 @@ exports.postOrder = (req, res, next) => {
           })
         )
       )
-    ).then((result) => {
-      console.log('result: ', result)
-      res.redirect('/orders')
-    })
+    )
+    .then(() => fetchedCart.setProducts(null))
+    .then(() => res.redirect('/orders'))
     .catch((err) => console.log(err))
 }
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders',
-  })
-}
-
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout',
+  req.user.getOrders({ include: ['products'] }).then((orders) => {
+    res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders',
+      orders,
+    })
   })
 }
