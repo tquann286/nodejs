9735173302816a1765e151f06ed7fd62c3b24536
tquann286 <quantrung286@gmail.com>
2023-12-25 13:33:08 +0700

    197

--- controllers/shop.js
+++ controllers/shop.js
@@ -57,30 +57,35 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId
-  let fetchedCart
-  let newQuantity = 1
-  req.user
-    .getCart()
-    .then((cart) => {
-      fetchedCart = cart
-      return cart.getProducts({ where: { id: prodId } })
-    })
-    .then(([product]) => {
-      if (product) {
-        const oldQuantity = product.cartItem.quantity
-        newQuantity = oldQuantity + 1
-        return product
-      }
+  Product.findById(prodId).then(product => {
+    return req.user.addToCart(product)
+  }).then(result => {
+    console.log('result: ', result)
+  })
+  // let fetchedCart
+  // let newQuantity = 1
+  // req.user
+  //   .getCart()
+  //   .then((cart) => {
+  //     fetchedCart = cart
+  //     return cart.getProducts({ where: { id: prodId } })
+  //   })
+  //   .then(([product]) => {
+  //     if (product) {
+  //       const oldQuantity = product.cartItem.quantity
+  //       newQuantity = oldQuantity + 1
+  //       return product
+  //     }
 
-      return Product.findByPk(prodId)
-    })
-    .then((product) => {
-      return fetchedCart.addProduct(product, { through: { quantity: newQuantity } })
-    })
-    .then(() => {
-      res.redirect('/cart')
-    })
-    .catch((err) => console.log(err))
+  //     return Product.findByPk(prodId)
+  //   })
+  //   .then((product) => {
+  //     return fetchedCart.addProduct(product, { through: { quantity: newQuantity } })
+  //   })
+  //   .then(() => {
+  //     res.redirect('/cart')
+  //   })
+  //   .catch((err) => console.log(err))
 }
 
 exports.postCartDeleteProduct = (req, res, next) => {
--- models/user.js
+++ models/user.js
@@ -24,7 +24,7 @@ class User {
   }
 
   addToCart(product) {
-    const updatedCart = { items: [{ ...product, quantity: 1 }] }
+    const updatedCart = { items: [{ productId: new ObjectId(product._id), quantity: 1 }] }
     const db = getDb()
     return db.collection('users').updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: updatedCart } })
   }
