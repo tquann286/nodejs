fd5dac5eaac265a15b76139b695f257704c04a53
tquann286 <quantrung286@gmail.com>
2023-12-25 21:47:03 +0700

    224

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product')
+const Order = require('../models/order')
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -74,7 +75,23 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.postOrder = (req, res, next) => {
   req.user
-    .addOrder()
+    .populate('cart.items.productId')
+    .then((user) => {
+      const products = (user?.cart?.items || []).map((i) => ({
+        quantity: i.quantity,
+        product: i.productId,
+      }))
+
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user,
+        },
+        products,
+      })
+
+      return order.save()
+    })
     .then(() => res.redirect('/orders'))
     .catch((err) => console.log(err))
 }
