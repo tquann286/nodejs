63e48813c1d3aa1abb5700d4a1f3bb9a23cc68da
tquann286 <quantrung286@gmail.com>
2023-12-25 21:48:42 +0700

    225

--- controllers/shop.js
+++ controllers/shop.js
@@ -79,7 +79,7 @@ exports.postOrder = (req, res, next) => {
     .then((user) => {
       const products = (user?.cart?.items || []).map((i) => ({
         quantity: i.quantity,
-        product: i.productId,
+        product: { ...i?.productId?._doc },
       }))
 
       const order = new Order({
