dcaa07182b6648b5a9cf4bdd374291aeaeefab26
tquann286 <quantrung286@gmail.com>
2023-12-26 19:46:42 +0700

    235

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
 
 const errorController = require('./controllers/error')
 const User = require('./models/user')
@@ -13,13 +14,16 @@ app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 const adminRoutes = require('./routes/admin')
-const authRoutes = require('./routes/auth');
+const authRoutes = require('./routes/auth')
 const shopRoutes = require('./routes/shop')
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(cookieParser())
 
 app.use((req, res, next) => {
+  req.isLoggedIn = req.cookies.loggedIn === 'true'
   User.findById('65897f5010e884d405bae320')
     .then((user) => {
       req.user = user
