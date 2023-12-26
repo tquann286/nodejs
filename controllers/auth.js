b20a23beed3658744be2b2cad0c6c012c7bced46
tquann286 <quantrung286@gmail.com>
2023-12-26 22:48:02 +0700

    257

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,12 +10,27 @@ exports.getLogin = (req, res, next) => {
 }
 
 exports.postLogin = (req, res, next) => {
-  User.findById('65897f5010e884d405bae320')
+  const { email, password } = req.body
+
+  User.findOne({ email })
     .then((user) => {
-      req.session.isLoggedIn = true
-      req.session.user = user
-      req.session.save(() => {
-        res.redirect('/')
+      if (!user) {
+        return res.redirect('/login')
+      }
+
+      bcrypt.compare(password, user.password).then((doMatch) => {
+        if (doMatch) {
+          req.session.isLoggedIn = true
+          req.session.user = user
+          return req.session.save(() => {
+            res.redirect('/')
+          })
+        }
+
+        res.redirect('/login')
+      }).catch((err) => {
+        console.log('err: ', err)
+        res.redirect('/login')
       })
     })
     .catch((err) => console.log(err))
