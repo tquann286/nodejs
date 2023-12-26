b1a815d3bfe51d73ecb998fbddf3234bf1c7306d
tquann286 <quantrung286@gmail.com>
2023-12-26 20:09:10 +0700

    239

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
+const session = require('express-session')
 
 const errorController = require('./controllers/error')
 const User = require('./models/user')
@@ -21,6 +22,7 @@ const shopRoutes = require('./routes/shop')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(cookieParser())
+app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false }))
 
 app.use((req, res, next) => {
   req.isLoggedIn = req.cookies.loggedIn === 'true'
