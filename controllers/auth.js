00988cb70516c1869df3b24e44bf794c790e5ab2
tquann286 <quantrung286@gmail.com>
2023-12-26 22:08:32 +0700

    253

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,6 +20,16 @@ exports.postLogin = (req, res, next) => {
     .catch((err) => console.log(err))
 }
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false,
+  })
+}
+
+exports.postSignup = (req, res, next) => {}
+
 exports.postLogout = (req, res, next) => {
   req.session.destroy(() => {
     res.redirect('/')
