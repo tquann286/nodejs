ac0b4ee241f33cd89cd7c9555247e195dca50858
tquann286 <quantrung286@gmail.com>
2023-12-30 16:26:07 +0700

    293

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const nodemailer = require('nodemailer')
 const crypto = require('crypto')
+const { validationResult } = require('express-validator')
 
 const User = require('../models/user')
 
@@ -62,6 +63,16 @@ exports.postSignup = (req, res, next) => {
   const { email } = req.body
   const { password } = req.body
   const { confirmPassword } = req.body
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    console.log(errors.array())
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg,
+    })
+  }
 
   User.findOne({ email }).then((userDoc) => {
     if (userDoc) {
