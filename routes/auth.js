cdce84a5ffc5e08112c0758e9ab99d3f19fcd7f9
tquann286 <quantrung286@gmail.com>
2023-12-30 16:35:45 +0700

    295

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { check } = require('express-validator')
+const { check, body } = require('express-validator')
 
 const authController = require('../controllers/auth')
 
@@ -22,6 +22,7 @@ router.post(
       }
       return true
     }),
+  body('password', 'Please enter a password with only numbers and text and at least 5 characters.').isLength({ min: 5 }).isAlphanumeric(),
   authController.postSignup
 )
 
