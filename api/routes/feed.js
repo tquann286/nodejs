679233edd1feaf07f7914e030733213c9bc3ae9d
tquann286 <quantrung286@gmail.com>
2024-01-06 21:51:14 +0700

    384

--- api/routes/feed.js
+++ api/routes/feed.js
@@ -6,11 +6,10 @@ const feedController = require('../controllers/feed')
 
 router.get('/posts', feedController.getPosts)
 
-router.post('/post', [
-  body('title').trim().isLength({ min: 5 }),
-  body('content').trim().isLength({ min: 5 }),
-], feedController.createPost)
+router.post('/post', [body('title').trim().isLength({ min: 5 }), body('content').trim().isLength({ min: 5 })], feedController.createPost)
 
 router.get('/post/:postId', feedController.getPost)
+
+router.put('/post/:postId', [body('title').trim().isLength({ min: 5 }), body('content').trim().isLength({ min: 5 })], feedController.updatePost)
 
 module.exports = router
