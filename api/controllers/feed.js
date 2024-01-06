40f0e1118960abd1c90708f93d288070c0d77e99
tquann286 <quantrung286@gmail.com>
2024-01-06 19:14:14 +0700

    374

--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -1,9 +1,8 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }],
+    posts: [{ _id: '1', title: 'First Post', content: 'This is the first post!', imageUrl: 'images/duck.jpg', creator: { name: 'Quantrung' }, createdAt: new Date() }],
   })
 }
-
 
 exports.createPost = (req, res, next) => {
   const title = req.body.title
