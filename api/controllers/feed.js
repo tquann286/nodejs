399b89ec35eb94a31a657f2ed565082f77395917
tquann286 <quantrung286@gmail.com>
2024-01-06 20:29:50 +0700

    376

--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator')
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [{ _id: '1', title: 'First Post', content: 'This is the first post!', imageUrl: 'images/duck.jpg', creator: { name: 'Quantrung' }, createdAt: new Date() }],
@@ -5,6 +7,15 @@ exports.getPosts = (req, res, next) => {
 }
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: 'Validation failed, entered data is incorrect.',
+      errors: errors.array(),
+    })
+  }
+
   const title = req.body.title
   const content = req.body.content
   // Create post in db
