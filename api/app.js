f48c40742e7328c3fa982e83ec9ef2610f56a1f3
tquann286 <quantrung286@gmail.com>
2024-01-06 20:47:59 +0700

    379

--- api/app.js
+++ api/app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
@@ -8,12 +9,20 @@ const feedRoutes = require('./routes/feed')
 const app = express()
 
 app.use(bodyParser.json())
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use(cors())
 
 app.use(express.json())
 
 app.use('/feed', feedRoutes)
+
+app.use((error, req, res, next) => {
+  console.log('error: ', error)
+  const status = error.statusCode || 500
+  const message = error.message
+  res.status(status).json({ message: message })
+})
 
 mongoose
   .connect('mongodb+srv://quantrung286:Trungquan2806@cluster0.uknlqmo.mongodb.net/messages?retryWrites=true&w=majority')
--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -3,19 +3,25 @@ const { validationResult } = require('express-validator')
 const Post = require('../models/post')
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [{ _id: '1', title: 'First Post', content: 'This is the first post!', imageUrl: 'images/duck.jpg', creator: { name: 'Quantrung' }, createdAt: new Date() }],
-  })
+  Post.find()
+    .then((posts) => {
+      res.status(200).json({
+        message: 'Fetched posts successfully.',
+        posts: posts,
+      })
+    })
+    .catch((err) => {
+      console.log('err: ', err)
+    })
 }
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req)
 
   if (!errors.isEmpty()) {
-    return res.status(422).json({
-      message: 'Validation failed, entered data is incorrect.',
-      errors: errors.array(),
-    })
+    const error = new Error('Validation failed, entered data is incorrect.')
+    error.statusCode = 422
+    throw error
   }
 
   const title = req.body.title
@@ -37,6 +43,9 @@ exports.createPost = (req, res, next) => {
       })
     })
     .catch((err) => {
-      console.log(err)
+      if (!err.statusCode) {
+        err.statusCode = 500
+      }
+      next(err)
     })
 }
