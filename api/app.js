f0f9db7b284a51a773b95e600b3c4b6e6f6ad8ab
tquann286 <quantrung286@gmail.com>
2024-01-06 17:18:23 +0700

    368

--- api/app.js
+++ api/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const feedRoutes = require('./routes/feed')
 
 const app = express()
+
+app.use(bodyParser.json())
+
+app.use(cors())
 
 app.use(express.json())
 
--- api/routes/feed.js
+++ api/routes/feed.js
@@ -5,4 +5,6 @@ const feedController = require('../controllers/feed')
 
 router.get('/posts', feedController.getPosts)
 
+router.post('/post', feedController.createPost)
+
 module.exports = router
