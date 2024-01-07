60b9d34e6ecb9c63345b238e9f001ee20ba0ddee
tquann286 <quantrung286@gmail.com>
2024-01-07 19:38:28 +0700

    390

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -8,4 +8,6 @@ const authController = require('../controllers/auth')
 
 router.put('/signup', [body('email').isEmail().withMessage('Please enter a valid email.').normalizeEmail(), body('password').trim().isLength({ min: 5 }), body('name').trim().not().isEmpty()], authController.signup)
 
+router.post('/login', [body('email').isEmail().withMessage('Please enter a valid email.').normalizeEmail(), body('password').trim().isLength({ min: 5 })], authController.login)
+
 module.exports = router
