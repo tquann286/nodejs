b54921547163ac382f9f155e7070a8ee4b6bf4c0
tquann286 <quantrung286@gmail.com>
2024-01-07 20:25:26 +0700

    394

--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -4,6 +4,7 @@ const path = require('path')
 const { validationResult } = require('express-validator')
 
 const Post = require('../models/post')
+const User = require('../models/user')
 
 exports.getPosts = (req, res, next) => {
   const currentPage = req.query.page || 1
@@ -51,20 +52,30 @@ exports.createPost = (req, res, next) => {
   const imageUrl = req.file.path
   const title = req.body.title
   const content = req.body.content
+  let creator
   const post = new Post({
     title,
     content,
     imageUrl,
-    creator: { name: 'Quantrung' },
+    creator: req.userId,
   })
 
   post
     .save()
     .then((result) => {
-      console.log('result: ', result)
+      return User.findById(req.userId)
+    })
+    .then((user) => {
+      creator = user
+      user.posts.push(post)
+
+      return user.save()
+    })
+    .then(() => {
       res.status(201).json({
         message: 'Post created successfully!',
-        post: result,
+        post: post,
+        creator: { _id: creator._id, name: creator.name },
       })
     })
     .catch((err) => {
