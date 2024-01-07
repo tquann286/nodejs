590ced8f1fcd7c835dcaa233c1ddf7e8bffe9f26
tquann286 <quantrung286@gmail.com>
2024-01-07 20:49:57 +0700

    395

--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -138,6 +138,12 @@ exports.updatePost = (req, res, next) => {
         throw error
       }
 
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized!')
+        error.statusCode = 403
+        throw error
+      }
+
       if (imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl)
       }
@@ -167,6 +173,12 @@ exports.deletePost = (req, res, next) => {
       if (!post) {
         const error = new Error('Could not find post.')
         error.statusCode = 404
+        throw error
+      }
+
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized!')
+        error.statusCode = 403
         throw error
       }
 
