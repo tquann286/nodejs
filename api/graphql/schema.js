ce71e8044a6823950a884766fa959cdc1b001123
tquann286 <quantrung286@gmail.com>
2024-01-11 22:29:08 +0700

    423

--- api/graphql/schema.js
+++ api/graphql/schema.js
@@ -1,21 +1,35 @@
 const { buildSchema } = require('graphql')
 
 module.exports = buildSchema(`
-  type TestData {
-    text: String!
-    views: Int!
+  type Post {
+    _id: ID!
+    title: String!
+    content: String!
+    imageUrl: String!
+    creator: User!
+    createdAt: String!
+    updatedAt: String!
   }
 
-  type RootQuery {
-    hello: TestData
+  type User {
+    _id: ID!
+    email: String!
+    name: String!
+    password: String
+    posts: [Post!]!
+  }
+
+  input UserInputData {
+    email: String!
+    name: String!
+    password: String!
   }
 
   type RootMutation {
-    hello: TestData
+    createUser(userInput: UserInputData): User!
   }
 
   schema {
-    query: RootQuery
     mutation: RootMutation
   }
 `)
