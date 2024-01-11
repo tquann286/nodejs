c73bba5ce192383f9689aff82fdd88f585541664
tquann286 <quantrung286@gmail.com>
2024-01-11 22:46:42 +0700

    425

--- api/graphql/resolvers.js
+++ api/graphql/resolvers.js
@@ -1,18 +1,33 @@
 const bcrypt = require('bcryptjs')
+const validator = require('validator')
 
 const User = require('../../models/user')
 
 module.exports = {
   createUser: async function ({ userInput }, req) {
-    const email = userInput.email
-    const name = userInput.name
+    const { email, name, password } = userInput
+
+    const errors = []
+    if (!validator.isEmail(email)) {
+      errors.push({ message: 'E-Mail is invalid.' })
+    }
+    if (validator.isEmpty(password) || !validator.isLength(password, { min: 5 })) {
+      errors.push({ message: 'Password too short!' })
+    }
+
+    if (errors.length > 0) {
+      const error = new Error('Invalid input.')
+      error.data = errors
+      error.code = 422
+      throw error
+    }
 
     const existingUser = await User.findOne({ email })
     if (existingUser) {
       const error = new Error('User exists already!')
       throw error
     }
-    const hashedPw = await bcrypt.hash(userInput.password, 12)
+    const hashedPw = await bcrypt.hash(password, 12)
     const user = new User({
       email,
       name,
