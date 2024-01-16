595863d9395ae0bca8f803352338c064a5369c12
tquann286 <quantrung286@gmail.com>
2024-01-16 21:30:06 +0700

    474

--- api/test/auth-controller.js
+++ api/test/auth-controller.js
@@ -1,31 +1,48 @@
 const sinon = require('sinon')
+const mongoose = require('mongoose')
 
 const User = require('../models/user')
 const AuthController = require('../controllers/auth')
 
-// describe('Auth Controller', () => {
-//   it('should throw an error with code 500 if accessing the database fails', async (done) => {
-//     const { expect } = await require('chai')
+describe('Auth Controller', () => {
+  // it('should throw an error with code 500 if accessing the database fails', async (done) => {
+  //   const { expect } = await require('chai')
 
-//     sinon.stub(User, 'findOne')
-//     User.findOne.throws()
+  //   sinon.stub(User, 'findOne')
+  //   User.findOne.throws()
 
-//     const req = {
-//       body: {
-//         email: 'test@test.com',
-//         password: 'tester',
-//       },
-//     }
+  //   const req = {
+  //     body: {
+  //       email: 'test@test.com',
+  //       password: 'tester',
+  //     },
+  //   }
 
-//     AuthController.login(req, {}, () => {}).then((result) => {
-//       console.log('result: ', result)
-//       expect(result).to.be.an('error')
-//       expect(result).to.have.property('statusCode', 500)
-//       done()
-//     }).catch((err) => {
-//       done(err)
-//     })
+  //   AuthController.login(req, {}, () => {}).then((result) => {
+  //     console.log('result: ', result)
+  //     expect(result).to.be.an('error')
+  //     expect(result).to.have.property('statusCode', 500)
+  //     done()
+  //   }).catch((err) => {
+  //     done(err)
+  //   })
 
-//     User.findOne.restore()
-//   })
-// })
+  //   User.findOne.restore()
+  // })
+
+  it('should send a response with a valid user status for an existing user', async (done) => {
+    const { expect } = await require('chai')
+
+    mongoose
+      .connect('mongodb+srv://quantrung286:Trungquan2806@cluster0.uknlqmo.mongodb.net/test-messages?retryWrites=true&w=majority')
+      .then(() => {
+        const user = new User({
+          email: 'test@test.com',
+          password: 'tester',
+          name: 'Test',
+          posts: [],
+        })
+      })
+      .catch((err) => console.log(err))
+  })
+})
