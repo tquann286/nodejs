8e3b466d68e7baa43178c3a341d0abf2ec8421d7
tquann286 <quantrung286@gmail.com>
2024-01-16 20:57:01 +0700

    471

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,5 +1,7 @@
 const { describe } = require('mocha')
 const authMiddleware = require('../middleware/is-auth')
+const jwt = require('jsonwebtoken')
+const sinon = require('sinon')
 
 describe('Auth middleware', () => {
   it('should throw an error if no authorization header is present', async () => {
@@ -10,21 +12,24 @@ describe('Auth middleware', () => {
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not authenticated.')
   })
 
+  it('should yield a userId after decoding the token', async () => {
+    const { expect } = await import('chai')
+    const req = {
+      get: () => 'Bearer zxc',
+    }
+    sinon.stub(jwt, 'verify')
+    jwt.verify.returns({ userId: 'abc' })
+
+    authMiddleware(req, {}, () => {})
+    expect(req).to.have.property('userId')
+    jwt.verify.restore()
+  })
+
   it('should throw an error if the authorization header is only one string', async () => {
     const { expect } = await import('chai')
     const req = {
       get: () => 'xyz',
     }
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw()
-  })
-
-  it('should yield a userId after decoding the token', async () => {
-    const { expect } = await import('chai')
-    const req = {
-      get: () => 'Bearer zxc',
-    }
-
-    authMiddleware(req, {}, () => {})
-    expect(req).to.have.property('userId')
   })
 })
